27d8f01bffbce1bb4bb1f6bcfeddb5bb0870dc6d
Henrique <henri.acle@gmail.com>
2018-08-30 13:35:05 -0300

    feat(menu) menu Adicionar ações de seleção ao menu

--- src/app/menu.js
+++ src/app/menu.js
@@ -6,6 +6,10 @@ class MainMenu extends Phaser.Scene {
             key: 'MainMenu',
             pixelArt: true
         })
+
+        this.menuItens = [];
+        this.selectedMenu = null;
+        this.activeIndex = 0;
     }
 
     preload() {
@@ -23,8 +27,8 @@ class MainMenu extends Phaser.Scene {
         rokLogo.setX(this.sys.game.config.width / 2 - (rokLogo.width * rokLogo.scaleX / 2));
         rokLogo.setY(50);
 
-        this.make.text({
-            x: this.sys.game.config.width / 2,
+        const newGame = this.make.text({
+            x: this.sys.game.config.width / 2 - 40,
             y: this.sys.game.config.height / 2,
             text: 'Novo jogo',
             style: {
@@ -32,29 +36,81 @@ class MainMenu extends Phaser.Scene {
                 fill: '#fff'
             }
         });
-        
-        this.make.text({
-            x: this.sys.game.config.width / 2,
-            y: this.sys.game.config.height / 2,
+
+        newGame.setInteractive();
+
+        newGame.on('pointerdown', function() {
+            console.log('start game');
+        });
+
+        const continuar = this.make.text({
+            x: this.sys.game.config.width / 2 - 40,
+            y: this.sys.game.config.height / 2 + 40,
             text: 'Continuar',
             style: {
                 font: '20px monospace',
                 fill: '#fff'
             }
         });
-        
-        this.make.text({
-            x: this.sys.game.config.width / 2,
-            y: this.sys.game.config.height / 2,
+
+        const opcoes = this.make.text({
+            x: this.sys.game.config.width / 2 - 40,
+            y: this.sys.game.config.height / 2 + 80,
             text: 'Opções',
             style: {
                 font: '20px monospace',
                 fill: '#fff'
             }
-        });        
+        });     
+        
+        this.menuItens = [newGame, continuar, opcoes];
+        this.selectedMenu = this.menuItens[this.activeIndex];
+        this.selectedMenu.setColor('#00ff00');
+
+        this.input.keyboard.on('keydown_UP', function() {
+            this.selectedMenu.setColor('#ffffff');
+            this.activeIndex--;
+            
+            if (this.activeIndex < 0) {
+                this.activeIndex = 0;
+            }
+
+            this.selectedMenu = this.menuItens[this.activeIndex];
+            this.selectedMenu.setColor('#00ff00');
+        }, this);
+
+        this.input.keyboard.on('keydown_DOWN', function() {
+            this.selectedMenu.setColor('#ffffff');
+            this.activeIndex++;
+            
+            if (this.activeIndex > 2) {
+                this.activeIndex = 2;
+            }
+
+            this.selectedMenu = this.menuItens[this.activeIndex];
+            this.selectedMenu.setColor('#00ff00');        
+        }, this);
+
+        this.input.keyboard.on('keydown_ENTER', function() {
+            switch(this.activeIndex) {
+                case(0):
+                    this.cameras.main.fadeOut(1000, 0, 0, 0, function() {
+                    }, this);
+                    // this.scene.start('faseum');
+                    break;
+                case(1):
+                    console.log('continuar jogo');
+                    break;
+                case(2):
+                    console.log('abrir opções');
+            }
+        }, this);
     }
 
     update() {
+        if(this.cameras.main.fadeEffect.isComplete) {
+            this.scene.start('faseum');
+        }
 
     }
 }
