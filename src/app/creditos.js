1d9abc66258ed847ea86f0336fd787b0d764d040
Henrique <henri.acle@gmail.com>
2018-08-30 06:55:32 -0300

    feat(texto) ROK-1 adicionado texto na tela de creditos

--- src/app/creditos.js
+++ src/app/creditos.js
@@ -13,7 +13,15 @@ class Creditos extends Phaser.Scene {
     }
 
     create() {
-
+        this.make.text({
+            x: this.sys.game.config.width / 2,
+            y: this.sys.game.config.height / 2,
+            text: 'Créditos',
+            style: {
+                font: '20px monospace',
+                fill: '#fff'
+            }
+        })
     }
 
     update() {
