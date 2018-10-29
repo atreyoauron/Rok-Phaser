98359072ea153234f404bcfcad2268911138509d
Henrique <henri.acle@gmail.com>
2018-10-29 15:54:13 -0300

    Removido god mode

--- src/app/fases/fase7.js
+++ src/app/fases/fase7.js
@@ -3,132 +3,216 @@ import CrowSpawner from '../prefabs/crow-spawner.js';
 import CheckPoint from '../prefabs/checkpoint.js';
 
 class FaseSete extends Phaser.Scene {
-    constructor() {
-        super({
-            key: 'fasesete',
-            pixelArt: true,
-            physics: {
-                arcade: {
-                    gravity: { y: 700 },
-                    debug: false,
-                    // tileBias: 120,
-                }
-            }
-        })
-
-        this.odin;
-        this.common;
-    }
-
-    init(config) {
-        this.common = this.scene.get('boot');
-        this.ui = this.scene.get('userInterface');
-        this.ui.events.emit('damageTaken', 0);
-
-        this.odin = this.common.odin;
-        this.scene.stop('faseseis');
-        this.odin.resetSpearGroup();
-
-        if (config) {
-            this.odin.x = config.odinx;
-            this.odin.y = config.odiny;
-            this.physics.world.enable(this.odin);
+  constructor() {
+    super({
+      key: 'fasesete',
+      pixelArt: true,
+      physics: {
+        arcade: {
+          gravity: {
+            y: 700
+          },
+          debug: false,
+          // tileBias: 120,
         }
+      }
+    })
+
+    this.odin;
+    this.common;
+  }
+
+  init(config) {
+    this.common = this.scene.get('boot');
+    this.ui = this.scene.get('userInterface');
+    this.ui.events.emit('damageTaken', 0);
+
+    this.odin = this.common.odin;
+    this.scene.stop('faseseis');
+    this.odin.resetSpearGroup();
+
+    if (config) {
+      this.odin.x = config.odinx;
+      this.odin.y = config.odiny;
+      this.physics.world.enable(this.odin);
+      console.log('caindo aqui');
+      this.odin.resetSpearGroup();
     }
+  }
 
-    preload() {
+  preload() {
 
+  }
+
+  create() {
+    this.cameras.main.setBackgroundColor('rgba(10, 230, 255, 1)');
+
+    var map = this.add.tilemap('fase_7');
+
+    var tileset = map.addTilesetImage('plataformas');
+    this.ground = map.createDynamicLayer('plataformas', tileset);
+    this.ground.setCollisionByProperty({
+      collider: true
+    });
+    this.physics.add.collider(this.odin, [this.ground], function () {
+      this.odin.resetJump();
+    }, null, this);
+    this.odin = this.add.existing(this.odin);
+
+    const spearItem = this.physics.add.staticImage(565, 213, 'spear-item');
+
+    this.physics.add.overlap(this.odin, spearItem, function (odin, spear) {
+      this.odin.getSpear();
+      spear.destroy();
+    }, null, this);
+
+    this.crows = new CrowSpawner({
+      scene: this,
+      groupConfig: {
+        defaultKey: 'barril',
+        maxSize: 15,
+      },
+      groupMultipleConfig: {},
+      customConfig: {
+        x: 400,
+        y: 300,
+        bounce: {
+          x: 1,
+          y: 1
+        },
+        speedDirection: {
+          x: 0,
+          y: 0
+        },
+        colliders: [this.ground],
+        overlap: this.odin,
+      }
+    });
+
+    this.crows.createCrow({
+      x: 530,
+      y: 214
+    }, {
+      x: 0,
+      y: 50
+    });
+
+    this.crows.createCrow({
+      x: 300,
+      y: 320
+    }, {
+      x: 0,
+      y: 50
+    });
+    this.crows.createCrow({
+      x: 330,
+      y: 300
+    }, {
+      x: 0,
+      y: 50
+    });
+    this.crows.createCrow({
+      x: 360,
+      y: 280
+    }, {
+      x: 0,
+      y: 50
+    });
+    this.crows.createCrow({
+      x: 400,
+      y: 260
+    }, {
+      x: 0,
+      y: 50
+    });
+
+    this.crows.createCrow({
+      x: 25,
+      y: 280
+    }, {
+      x: -50,
+      y: 0
+    });
+    this.crows.createCrow({
+      x: 120,
+      y: 300
+    }, {
+      x: 50,
+      y: 0
+    });
+
+    this.crows.createCrow({
+      x: 120,
+      y: 67
+    }, {
+      x: -50,
+      y: 0
+    });
+    this.crows.createCrow({
+      x: 160,
+      y: 100
+    }, {
+      x: 50,
+      y: 0
+    });
+
+    this.crows.createCrow({
+      x: 280,
+      y: 60
+    }, {
+      x: -50,
+      y: 0
+    });
+    this.crows.createCrow({
+      x: 410,
+      y: 60
+    }, {
+      x: 50,
+      y: 0
+    });
+
+    this.crows.createCrow({
+      x: 280,
+      y: 90
+    }, {
+      x: -50,
+      y: 0
+    });
+    this.crows.createCrow({
+      x: 410,
+      y: 90
+    }, {
+      x: 50,
+      y: 0
+    });
+
+
+    const checkpoint = new CheckPoint({
+      scene: this,
+      x: 494,
+      y: 142,
+      key: 'switch-block'
+    });
+
+    this.physics.add.overlap(this.odin, checkpoint, (over1, over2) => {
+      this.ui.getCheckpoint(
+        511,
+        144,
+        'fasesete');
+      this.ui.events.emit('damageTaken', 0);
+    }, null, this);
+  }
+
+  update() {
+    this.odin.checkCursorMoviment(this.common);
+
+    if (this.odin.x >= 568 && this.odin.x <= 640 && this.odin.y > 360) {
+      this.scene.start('faseseis', {
+        odinx: this.odin.x,
+        odiny: 0
+      });
     }
-
-    create() {
-        this.cameras.main.setBackgroundColor('rgba(10, 230, 255, 1)');
-
-        var map = this.add.tilemap('fase_7');
-
-        var tileset = map.addTilesetImage('plataformas');
-        this.ground = map.createDynamicLayer('plataformas', tileset);
-        this.ground.setCollisionByProperty({ collider: true });
-        this.physics.add.collider(this.odin, [this.ground], function() {
-            this.odin.resetJump();
-        }, null, this);
-        this.odin = this.add.existing(this.odin);
-
-        const spearItem = this.physics.add.staticImage(565, 213, 'spear-item');
-
-        this.physics.add.overlap(this.odin, spearItem, function(odin, spear) {
-            this.odin.getSpear();
-            spear.destroy();
-        }, null, this);
-
-        this.crows = new CrowSpawner({
-            scene: this,
-            groupConfig: {
-                defaultKey: 'barril',
-                maxSize: 15,
-            },
-            groupMultipleConfig: {},
-            customConfig: {
-                x: 400,
-                y: 300,
-                bounce: {
-                    x: 1,
-                    y: 1
-                },
-                speedDirection: {
-                    x: 0,
-                    y: 0
-                },
-                colliders: [this.ground],
-                overlap: this.odin,
-            }
-        });
-
-        this.crows.createCrow({x: 530, y: 214},{ x: 0, y: 50});
-
-        this.crows.createCrow({x: 300, y: 320},{ x: 0, y: 50});
-        this.crows.createCrow({x: 330, y: 300},{ x: 0, y: 50});
-        this.crows.createCrow({x: 360, y: 280},{ x: 0, y: 50});
-        this.crows.createCrow({x: 400, y: 260},{ x: 0, y: 50});
-
-        this.crows.createCrow({x: 25, y: 280},{ x: -50, y: 0});
-        this.crows.createCrow({x: 120, y: 300},{ x: 50, y: 0});
-
-        this.crows.createCrow({x: 120, y: 67},{ x: -50, y: 0});
-        this.crows.createCrow({x: 160, y: 100},{ x: 50, y: 0});
-
-        this.crows.createCrow({x: 280, y: 60},{ x: -50, y: 0});
-        this.crows.createCrow({x: 410, y: 60},{ x: 50, y: 0});
-
-        this.crows.createCrow({x: 280, y: 90},{ x: -50, y: 0});
-        this.crows.createCrow({x: 410, y: 90},{ x: 50, y: 0});
-
-
-        const checkpoint = new CheckPoint({
-          scene: this,
-          x: 494,
-          y: 142,
-          key: 'switch-block'
-        });
-
-        this.physics.add.overlap(this.odin, checkpoint, (over1, over2) => {
-          this.ui.getCheckpoint(
-            511,
-            144,
-            'faseseis');
-          this.ui.events.emit('damageTaken', 0);
-        }, null, this);
-    }
-
-    update() {
-        this.odin.checkCursorMoviment(this.common);
-
-        if (this.odin.x >= 568 && this.odin.x <= 640 && this.odin.y > 360) {
-            this.scene.start('faseseis', {
-                odinx: this.odin.x,
-                odiny: 0
-            });
-        }
-    }
+  }
 }
 
 export default FaseSete;
